3d3cf976942b29070308052c0c5e6fa7c5c2f638
techslb <techslb@outlook.com>
2025-10-26 12:09:33 -0500

    Add files via upload

---
+++ src/blackjack/Main.java
@@ -0,0 +1,12 @@
+package blackjack;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Midnight Blackjack");
+		
+		//Scanner input = new Scanner(System.in);
+	}
+
+}
