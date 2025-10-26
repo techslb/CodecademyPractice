f71292009e2f012e62106b3c9e2476eb9a9f6e48
techslb <techslb@outlook.com>
2025-10-26 12:38:44 -0500

    Add files via upload
    Updating the Main class to test the Dealer class with scaffolding.

--- src/blackjack/Main.java
+++ src/blackjack/Main.java
@@ -5,6 +5,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Midnight Blackjack");
+		Dealer newDealer = new Dealer(52, 0, "none");
+		
+		System.out.println(newDealer.numberOfCards);
 		
 		//Scanner input = new Scanner(System.in);
 	}
